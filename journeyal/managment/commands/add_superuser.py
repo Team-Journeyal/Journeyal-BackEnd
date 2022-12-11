a91d48adff5b3556ebd8d14dee2c2866d16f4681
ThatguyscottieC <officialscottiechambers@gmail.com>
2022-12-11 10:57:46 -0500

    setup up

---
+++ journeyal/managment/commands/add_superuser.py
@@ -0,0 +1,31 @@
+# your-app/management/commands/add_superuser.py
+import random
+
+from django.core.management.base import BaseCommand
+
+# Change this to reflect your app's name and name of your custom User model
+from journeyal.models import User
+from journeyal import settings  # Change this to reflect your app's name
+
+
+# To run this management command:
+# python manage.py add_superuser
+class Command(BaseCommand):
+    help = "Create a superuser in production"
+
+    def handle(self, *args, **options):
+        if not settings.DEBUG:
+            user, created = User.objects.get_or_create(
+                username=settings.DJANGO_SUPERUSER_USERNAME
+            )
+            if created:
+                user.email = settings.DJANGO_SUPERUSER_EMAIL
+                user.set_password(settings.DJANGO_SUPERUSER_PASSWORD)
+                user.is_superuser = True
+                user.save()
+                msg = self.style.SUCCESS(
+                    f"Superuser {settings.DJANGO_SUPERUSER_USERNAME} added to database.")
+            else:
+                msg = self.style.WARNING(
+                    f"Superuser {settings.DJANGO_SUPERUSER_USERNAME} already exists.")
+            self.stdout.write(msg)
