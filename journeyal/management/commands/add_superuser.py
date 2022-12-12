e7fa57965e8e432335668d7744edcd606824a5c7
Ray Young <ray.arden.young@gmail.com>
2022-12-12 18:29:19 -0500

    fixing is_staff in superuser commands

--- journeyal/management/commands/add_superuser.py
+++ journeyal/management/commands/add_superuser.py
@@ -18,14 +18,9 @@ class Command(BaseCommand):
             user, created = User.objects.get_or_create(
                 username=settings.DJANGO_SUPERUSER_USERNAME
             )
-            if created:
-                user.email = settings.DJANGO_SUPERUSER_EMAIL
-                user.set_password(settings.DJANGO_SUPERUSER_PASSWORD)
-                user.is_superuser = True
-                user.save()
-                msg = self.style.SUCCESS(
-                    f"Superuser {settings.DJANGO_SUPERUSER_USERNAME} added to database.")
-            else:
-                msg = self.style.WARNING(
-                    f"Superuser {settings.DJANGO_SUPERUSER_USERNAME} already exists.")
-            self.stdout.write(msg)
+
+            user.email = settings.DJANGO_SUPERUSER_EMAIL
+            user.set_password(settings.DJANGO_SUPERUSER_PASSWORD)
+            user.is_superuser = True
+            user.is_staff = True
+            user.save()
