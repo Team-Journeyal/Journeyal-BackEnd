3167c65c7da816bd4cd1b4f081462fa4491e66d1
ThatguyscottieC <officialscottiechambers@gmail.com>
2022-12-13 11:26:40 -0500

    changing models watch for migrations

--- journeyal/admin.py
+++ journeyal/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import User
+from .models import User, Journal, Event, Tag, Sticker
 from django.contrib.auth.admin import UserAdmin
 # Register your models here.
 
 admin.site.register(User, UserAdmin)
+admin.site.register(Journal)
+admin.site.register(Event)
+admin.site.register(Tag)
+admin.site.register(Sticker)
--- journeyal/models.py
+++ journeyal/models.py
@@ -7,32 +7,37 @@ class User(AbstractUser):
     name = models.CharField(max_length=50, null=True, blank=True)
     # avatar = models.ImageField(upload_to="user_avatars", blank=True, null=True)
 
-class Day(models.Model):
+
+class Journal(models.Model):
     date = models.DateField()
-    journal = models.TextField(null=True, blank=True)
-    # image = models.TextField(null=True, blank=True)
-    stickers = models.ManyToManyField('Sticker')
-    tags = models.ManyToManyField('Tag')
-    
+    entry = models.TextField(null=True, blank=True)
+
     def __str__(self):
-        return self.date
+        return str(self.date)
+
 
 class Event(models.Model):
     name = models.CharField(max_length=100)
-    day = models.ForeignKey(Day, on_delete=models.CASCADE, related_name='events')
+    date = models.DateField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
+
 
 class Tag(models.Model):
     name = models.CharField(max_length=100)
+    date = models.DateField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
 
+
 class Sticker(models.Model):
     pass
+    # date = models.DateField()
     # image = models.TextField()
     # created_at = models.DateTimeField(auto_now_add=True)
+
+# add image down here
