44c5aa04a38b5e629d49e11cac8e74491a94831e
Ray Young <ray.arden.young@gmail.com>
2022-12-14 11:30:49 -0500

    updated models :')

--- journeyal/models.py
+++ journeyal/models.py
@@ -7,37 +7,30 @@ class User(AbstractUser):
     name = models.CharField(max_length=50, null=True, blank=True)
     # avatar = models.ImageField(upload_to="user_avatars", blank=True, null=True)
 
+    def __str__(self):
+        return self.username
+
+
+class Calendar(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='calendars')
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
 
 class Journal(models.Model):
+    calendar = models.ForeignKey(Calendar, on_delete=models.CASCADE, related_name='journals')
     date = models.DateField()
     entry = models.TextField(null=True, blank=True)
+    event = models.CharField(max_length=200, null=True, blank=True)
+    # image = models.ImageField(null=True, blank=True)
+    # stickers = 
 
     def __str__(self):
         return str(self.date)
 
-
-class Event(models.Model):
-    name = models.CharField(max_length=100)
-    date = models.DateField()
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.name
-
-
-class Tag(models.Model):
-    name = models.CharField(max_length=100)
-    date = models.DateField()
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.name
-
-
-class Sticker(models.Model):
+class Notification(models.Model):
     pass
-    # date = models.DateField()
-    # image = models.TextField()
-    # created_at = models.DateTimeField(auto_now_add=True)
 
-# add image down here
+class Follow(models.Model):
+    pass
