e810656b4e71e65eddaeab070f98c155a2dcc275
Ray Young <ray.arden.young@gmail.com>
2022-12-15 16:24:53 -0500

    reverting aws back to under render

--- config/settings.py
+++ config/settings.py
@@ -16,8 +16,8 @@ from corsheaders.defaults import default_headers
 env = environ.Env(
     # set casting, default value
     DEBUG=(bool, False),
-    USE_S3=(bool, False),
     RENDER=(bool, False),
+    USE_S3=(bool, False),
 )
 
 environ.Env.read_env()
@@ -150,21 +150,6 @@ if not DEBUG:
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-if env('USE_S3'):
-    AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
-    AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
-
-    AWS_DEFAULT_ACL = 'public-read'
-
-    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-    AWS_S3_OBJECT_PARAMETERS = {
-        'CacheControl': 'max-age=86400',
-    }
-    AWS_S3_FILE_OVERWRITE = False
-    AWS_QUERYSTRING_AUTH = False
-# This is for django-storages with boto3
-    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 if env("RENDER"):
     ALLOWED_HOSTS.append(env("RENDER_EXTERNAL_HOSTNAME"))
@@ -194,3 +179,19 @@ DJOSER = {
         'current_user': 'api.serializers.UserSerializer'
     },
 }
+
+if env('USE_S3'):
+    AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
+    AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
+
+    AWS_DEFAULT_ACL = 'public-read'
+
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    AWS_S3_OBJECT_PARAMETERS = {
+        'CacheControl': 'max-age=86400',
+    }
+    AWS_S3_FILE_OVERWRITE = False
+    AWS_QUERYSTRING_AUTH = False
+# This is for django-storages with boto3
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
