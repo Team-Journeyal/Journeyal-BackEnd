ff4a42025dc3758112f512ac173fcc7a5803a19f
ThatguyscottieC <officialscottiechambers@gmail.com>
2022-12-19 14:36:08 -0500

    change of the admin for tags

--- journeyal/admin.py
+++ journeyal/admin.py
@@ -8,3 +8,13 @@ admin.site.register(Journal)
 admin.site.register(Calendar)
 admin.site.register(Follow)
 admin.site.register(Notification)
+
+
+class MyModelAdmin(admin.ModelAdmin):
+    list_display = ['tag_list']
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).prefetch_related('tags')
+
+    def tag_list(self, obj):
+        return u", ".join(o.name for o in obj.tags.all())
