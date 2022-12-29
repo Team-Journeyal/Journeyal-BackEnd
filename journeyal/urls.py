c4f1006ca7c3b600226a6006ee82d065603454a0
ThatguyscottieC <officialscottiechambers@gmail.com>
2022-12-29 10:11:37 -0500

    deleting year reviews

--- journeyal/urls.py
+++ journeyal/urls.py
@@ -9,24 +9,17 @@ urlpatterns = [
     path('user/<int:pk>/', views.UserDetail.as_view(), name='user-detail'),
     path('calendar/', views.CalendarCreateView.as_view(), name='calendar'),
     path('calendars/', views.CalendarListView.as_view(), name='calendar'),
-    path('calendar/<int:pk>/', views.CalendarDetail.as_view(), name='calendar-detail'),
-    path('calendar/cover/<int:pk>/', views.CalCoverImageView.as_view(), name='calendar-cover'),
+    path('calendar/<int:pk>/', views.CalendarDetail.as_view(),
+         name='calendar-detail'),
+    path('calendar/cover/<int:pk>/',
+         views.CalCoverImageView.as_view(), name='calendar-cover'),
     path('journal/', views.JournalView.as_view(), name='journal'),
     path('journal/<int:pk>/', views.JournalDetail.as_view(), name='journal-detail'),
     # path('image/', views.JournalImageView.as_view(), name='journal-image'),
     path("auth/users/me/avatar/", views.UserAvatarView.as_view(), name='user_avatar'),
-    path('journal/YIR', views.YearInReviewView.as_view(), name='YearInReview'),
-    path('journal/Jan', views.JanFilter.as_view(), name='MonthInReview'),
-    path('journal/Feb', views.FebFilter.as_view(), name='MonthInReview'),
-    path('journal/Mar', views.MarFilter.as_view(), name='MonthInReview'),
-    path('journal/Apr', views.AprFilter.as_view(), name='MonthInReview'),
-    path('journal/May', views.MayFilter.as_view(), name='MonthInReview'),
-    path('journal/Jun', views.JunFilter.as_view(), name='MonthInReview'),
-    path('journal/Jul', views.JulFilter.as_view(), name='MonthInReview'),
-    path('journal/Oct', views.OctFilter.as_view(), name='MonthInReview'),
-    path('journal/Nov', views.NovFilter.as_view(), name='MonthInReview'),
-    path('journal/Dec', views.DecFilter.as_view(), name='MonthInReview'),
+
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
