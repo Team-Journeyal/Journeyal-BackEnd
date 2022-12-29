f033725bc17f0320c4dd5cbe57a383f056a6a1ed
ThatguyscottieC <officialscottiechambers@gmail.com>
2022-12-29 10:06:13 -0500

    adding back views

---
+++ journeyal/views.py
@@ -0,0 +1,100 @@
+from rest_framework import generics, parsers, filters, status
+from .models import User, Calendar, Journal
+from .serializers import UserSerializer, CalendarSerializer, JournalSerializer, TaggitSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from django.db.models import Q
+
+# Create your views here.
+
+
+class UserView(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = []
+
+
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = []
+
+
+class CalendarCreateView(generics.CreateAPIView):
+    queryset = Calendar.objects.all()
+    serializer_class = CalendarSerializer
+    permission_classes = []
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class CalendarListView(generics.ListAPIView):
+    queryset = Calendar.objects.all()
+    serializer_class = CalendarSerializer
+    permission_classes = []
+
+    def get_queryset(self):
+        return Calendar.objects.filter(Q(users=self.request.user) | Q(owner=self.request.user))
+
+
+class CalendarDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Calendar.objects.all()
+    serializer_class = CalendarSerializer
+    permission_classes = []
+
+
+class JournalView(generics.ListCreateAPIView):
+    queryset = Journal.objects.all()
+    serializer_class = JournalSerializer
+    # parser_classes = [parsers.MultiPartParser, parsers.FormParser]
+    permission_classes = []
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['tags__name']
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    def get_queryset(self):
+        return Journal.objects.filter(Q(calendar__users__id=self.request.user.id) | Q(calendar__owner=self.request.user))
+
+    def save(self, commit=True):
+        instance = Journal.objects.tags
+
+    # def create(self, request, *args, **kwargs):
+    #     try:
+    #         return super().create(request, *args, **kwargs)
+    #     except KeyError:
+    #         error_data = {
+    #             "error": "Please upload an image."
+    #         }
+    #         return Response(error_data, status=status.HTTP_400_BAD_REQUEST)
+
+
+class JournalDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Journal.objects.all()
+    serializer_class = JournalSerializer
+    permission_classes = []
+
+
+class UserAvatarView(generics.UpdateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    parser_classes = [parsers.FileUploadParser]
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
+
+
+class CalCoverImageView(generics.UpdateAPIView):
+    queryset = Calendar.objects.all()
+    serializer_class = CalendarSerializer
+    parser_classes = [parsers.FileUploadParser]
+    permission_classes = [IsAuthenticated]
+
+# class JournalImageView(generics.UpdateAPIView):
+#     queryset = Image.objects.all()
+#     serializer_class = ImageSerializer
+#     parser_classes = [parsers.MultiPartParser, parsers.FormParser]
+#     permission_classes = [IsAuthenticated]
