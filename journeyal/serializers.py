626e195bb98bec75f3f559932e5214b0c1236ca2
Ray Young <ray.arden.young@gmail.com>
2022-12-29 16:45:24 -0500

    multiple file upload, testing with AWS

--- journeyal/serializers.py
+++ journeyal/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import User, Calendar, Journal, JournalImage, Notification, Follow
+from .models import User, Calendar, Journal, JournalImage, JournalFile, Notification, Follow
 from taggit.serializers import (TagListSerializerField, TaggitSerializer)
 
 
@@ -22,22 +22,27 @@ class JournalImageSerializer(serializers.ModelSerializer):
         model = JournalImage
         fields = ['id', 'image']
 
+class JournalFileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = JournalFile
+        fields = ['id', 'file']
+
 
 class JournalSerializer(TaggitSerializer, serializers.ModelSerializer):
     tags = TagListSerializerField(required=False)
     user = serializers.SlugRelatedField(read_only=True, slug_field='username')
     journal_images = JournalImageSerializer(many=True, read_only=True)
+    journal_files = JournalFileSerializer(many=True, read_only=True)
     uploaded_images = serializers.ListField(
         child=serializers.ImageField(
-            max_length=1000000, allow_empty_file=True, use_url=False),
-        write_only=True,
-        required=False
-    )
+            max_length=1000000, allow_empty_file=True, use_url=False), write_only=True, required=False)
+    uploaded_files = serializers.ListField(
+        child=serializers.FileField(
+            max_length=1000000, allow_empty_file=True, use_url=False), write_only=True, required=False)
 
     class Meta:
         model = Journal
-        fields = ('id', 'date', 'entry', 'event', 'calendar',
-                  'tags', 'journal_images', 'uploaded_images', 'user')
+        fields = ('id', 'date', 'user', 'entry', 'event', 'calendar', 'tags', 'journal_images', 'uploaded_images', 'journal_files', 'uploaded_files')
 
     # def create(self, validated_data):
     #     uploaded_images = validated_data.pop('uploaded_images')
@@ -48,12 +53,19 @@ class JournalSerializer(TaggitSerializer, serializers.ModelSerializer):
 
     def update(self, instance, validated_data):
         uploaded_images = validated_data.pop('uploaded_images', None)
+        uploaded_files = validated_data.pop('uploaded_files', None)
         if uploaded_images:
             # self.clear_existing_images(instance) # if we want to clear existing images.
             journal_image_model_instance = [JournalImage(
                 journal=instance, image=image) for image in uploaded_images]
             JournalImage.objects.bulk_create(
                 journal_image_model_instance
+            )
+        if uploaded_files:
+            journal_file_model_instance = [JournalFile(
+                journal=instance, file=file) for file in uploaded_files]
+            JournalFile.objects.bulk_create(
+                journal_file_model_instance
             )
         return super().update(instance, validated_data)
 
