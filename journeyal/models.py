831b630d92d4df513800c87535452ffc0173c812
ThatguyscottieC <officialscottiechambers@gmail.com>
2023-01-11 15:52:42 -0500

    adding ordered by

--- journeyal/models.py
+++ journeyal/models.py
@@ -14,8 +14,10 @@ class User(AbstractUser):
 
 
 class Calendar(models.Model):
-    users = models.ManyToManyField(User, related_name='user_calendars', blank=True, null=True)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='owner_calendars')
+    users = models.ManyToManyField(
+        User, related_name='user_calendars', blank=True, null=True)
+    owner = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='owner_calendars')
     name = models.CharField(max_length=50)
     cal_image = models.ImageField(
         upload_to="cal_covers", blank=True, null=True)
@@ -30,7 +32,8 @@ class Calendar(models.Model):
 
 
 class Journal(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='journals')
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='journals')
     calendar = models.ForeignKey(
         Calendar, on_delete=models.CASCADE, related_name='journals')
     date = models.DateField()
@@ -39,23 +42,22 @@ class Journal(models.Model):
     tags = TaggableManager(blank=True)
 
 
+class meta:
+    ordering = ['date']
+
 
 def __str__(self):
     return str(self.date)
 
 
-class Notification(models.Model):
-    pass
-
-
-class Follow(models.Model):
-    pass
-
-
 class JournalImage(models.Model):
-    journal = models.ForeignKey(Journal, on_delete=models.CASCADE, related_name='journal_images')
-    image = models.ImageField(upload_to="journal_images", blank=True, null=True)
+    journal = models.ForeignKey(
+        Journal, on_delete=models.CASCADE, related_name='journal_images')
+    image = models.ImageField(
+        upload_to="journal_images", blank=True, null=True)
+
 
 class JournalFile(models.Model):
-    journal = models.ForeignKey(Journal, on_delete=models.CASCADE, related_name='journal_files')
+    journal = models.ForeignKey(
+        Journal, on_delete=models.CASCADE, related_name='journal_files')
     file = models.FileField(upload_to="journal_files", blank=True, null=True)
